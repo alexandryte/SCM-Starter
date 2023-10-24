67f984a09757f8031ceaf9cfcb0ea5d881edf579
alexandryte <75197322+alexandryte@users.noreply.github.com>
2023-10-25 03:26:26 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
@@ -7,41 +7,40 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [burnAmount, setBurnAmount] = useState("");
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async() => {
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
+      const account = await ethWallet.request({ method: "eth_accounts" });
       handleAccount(account);
     }
   }
 
   const handleAccount = (account) => {
     if (account) {
-      console.log ("Account connected: ", account);
+      console.log("Account connected: ", account);
       setAccount(account);
-    }
-    else {
+    } else {
       console.log("No account found");
     }
   }
 
-  const connectAccount = async() => {
+  const connectAccount = async () => {
     if (!ethWallet) {
       alert('MetaMask wallet is required to connect');
       return;
     }
-  
+
     const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
     handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
+
     getATMContract();
   };
 
@@ -49,17 +48,17 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
+
     setATM(atmContract);
   }
 
-  const getBalance = async() => {
+  const getBalance = async () => {
     if (atm) {
       setBalance((await atm.getBalance()).toNumber());
     }
   }
 
-  const deposit = async() => {
+  const deposit = async () => {
     if (atm) {
       let tx = await atm.deposit(1);
       await tx.wait()
@@ -67,7 +66,7 @@ export default function HomePage() {
     }
   }
 
-  const withdraw = async() => {
+  const withdraw = async () => {
     if (atm) {
       let tx = await atm.withdraw(1);
       await tx.wait()
@@ -75,18 +74,24 @@ export default function HomePage() {
     }
   }
 
+  const burn = async (amount) => {
+    if (atm) {
+      let tx = await atm.burn(amount);
+      await tx.wait()
+      getBalance();
+    }
+  }
+
   const initUser = () => {
-    // Check to see if user has Metamask
     if (!ethWallet) {
       return <p>Please install Metamask in order to use this ATM.</p>
     }
 
-    // Check to see if user is connected. If not, connect to their account
     if (!account) {
       return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
     }
 
-    if (balance == undefined) {
+    if (balance === undefined) {
       getBalance();
     }
 
@@ -96,22 +101,36 @@ export default function HomePage() {
         <p>Your Balance: {balance}</p>
         <button onClick={deposit}>Deposit 1 ETH</button>
         <button onClick={withdraw}>Withdraw 1 ETH</button>
+
+        <div>
+          <input
+            type="number"
+            min="0"
+            step="0.01"
+            placeholder="Amount to burn"
+            value={burnAmount}
+            onChange={(e) => setBurnAmount(e.target.value)}
+          />
+          <button onClick={() => burn(burnAmount)}>Burn</button>
+        </div>
       </div>
     )
   }
 
-  useEffect(() => {getWallet();}, []);
+  useEffect(() => { getWallet(); }, []);
 
   return (
-    <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
-      {initUser()}
+    <div style={{ backgroundColor: 'pink' }}>
+      <main className="container">
+        <header><h1>WELCOME TO ALEXANDRYTE'S ATM!</h1></header>
+        {initUser()}
+      </main>
       <style jsx>{`
         .container {
           text-align: center
         }
       `}
       </style>
-    </main>
+    </div>
   )
 }
